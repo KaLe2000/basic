5c96c285a58d0c46dbccb5be2b46820c30178a78
Nikita Talovskiy <talovskiy@gmail.com>
2024-11-13 21:33:04 +0700

    WIP TREES

---
+++ 1.types/trees/BSTree.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// struct Node - структура узла
+// typedef struct Node {} node; - альтернативное имя для структуры узла
+// используется для удобства. вместо struct Node newNode используем node newNode
+typedef struct Node {
+    int value;
+    struct Node* left;
+    struct Node* right;
+} node;
+
+// Функция для создания нового узла
+// объявляет указатель для структуры Node, выделяет память размером struct Node
+// возвращает указатель на память 
+node* createNode(int value) {
+    node* newNode = (node*)malloc(sizeof(node));
+
+    if (newNode == NULL) {
+        printf("Memory allocation failed!\n");
+        exit(EXIT_FAILURE);
+    }
+
+    newNode->value = value;
+    newNode->left = NULL;
+    newNode->right = NULL;
+
+    return newNode;
+}
+
+/*// Функция создания дерева заданной глубины
+node* createTree(node* root, int currentDepth, int maxDepth)*/
+
+// Функция для освобождения памяти дерева (рекурсивно)
+// todo для тестирования сетить в указатель NULL
+void clearTree(node* root) {
+    if (root != NULL) {
+        clearTree(root->left);
+        clearTree(root->right);
+        free(root);
+        // root = NULL;
+    }
+}
+
+// Поиск ноды для Бинарного Дерева Поиска
+node* findNodeBSTree(node* root, int value) {
+    if (root == NULL)
+    {
+        return NULL;
+    }
+
+    if (value < root->value)
+    {
+        return findNodeBSTree(root->left, value);
+    }
+
+    if (value > root->value)
+    {
+        return findNodeBSTree(root->right, value);
+    }
+
+    return root;
+}
+
+// Функция для обхода дерева в симметричном порядке
+// аргументом функции является указатель с типом struct Node
+void inorderTraversal(node* root) {
+    if (root != NULL) {
+        inorderTraversal(root->left);
+        printf("%d\n", root->value);
+        inorderTraversal(root->right);
+    }
+}
+
---
+++ 1.types/trees/testBSTree.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "BSTree.c"
+
+void testCreateNode() {
+    int assertVal = 1;
+    node* root = createNode(assertVal);
+
+    if (root->value == assertVal)
+    {
+        printf("testCreateNode success\n");
+    } else {
+        printf("testCreateNode failed\n");
+    }
+
+    clearTree(root);
+}
+
+void testClearTree() {
+    node* root = createNode(1);
+    clearTree(root);
+
+    if (root == NULL)
+    {
+        printf("testClearTree success\n");
+    } else {
+        printf("testClearTree failed\n");
+    }
+}
+
+void testFindNodeBSTree() {
+    int assertVal = 15;
+    node* bsTree = createNode(20);
+
+    bsTree->left = createNode(10);
+    bsTree->left->left = createNode(5);
+    bsTree->left->right = createNode(assertVal);
+
+    bsTree->right = createNode(30);
+    bsTree->right->left = createNode(25);
+    bsTree->right->right = createNode(35);
+
+    node* resultNode = findNodeBSTree(bsTree, assertVal);
+
+    if (resultNode->value == assertVal)
+    {
+        printf("testFindNodeBSTree success\n");
+    } else {
+        printf("testFindNodeBSTree failed\n");
+    }
+
+    clearTree(bsTree);
+}
+
+int main() {
+    testCreateNode();
+    // testClearTree(); // see todo clearTree()
+    testFindNodeBSTree();
+
+    return EXIT_SUCCESS;
+}
