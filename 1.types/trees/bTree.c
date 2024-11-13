c5bdd647407a133f6bd3ce1bf38e147e2ddc993f
Nikita Talovskiy <talovskiy@gmail.com>
2024-11-13 16:17:40 +0700

    WIP TREES

--- 1.types/trees/bTree.c
+++ 1.types/trees/bTree.c
@@ -28,6 +28,9 @@ node* createNode(int value) {
     return newNode;
 }
 
+/*// Функция создания дерева заданной глубины
+node* createTree(node* root, int currentDepth, int maxDepth)*/
+
 // Функция для освобождения памяти дерева (рекурсивно)
 void clearTree(node* root) {
     if (root != NULL) {
@@ -37,29 +40,62 @@ void clearTree(node* root) {
     }
 }
 
+// Поиск ноды для Бинарного Дерева Поиска
+node* findNodeBSTree(node* root, int value) {
+    if (root == NULL)
+    {
+        return NULL;
+    }
+
+    if (value < root->value)
+    {
+        return findNodeBSTree(root->left, value);
+    }
+
+    if (value > root->value)
+    {
+        return findNodeBSTree(root->right, value);
+    }
+
+    return root;
+}
+
 // Функция для обхода дерева в симметричном порядке
 // аргументом функции является указатель с типом struct Node
 void inorderTraversal(node* root) {
     if (root != NULL) {
         inorderTraversal(root->left);
-        printf("%d ", root->value);
+        printf("%d\n", root->value);
         inorderTraversal(root->right);
     }
 }
 
 int main() {
-    // node* tree = NULL;
     node* tree = createNode(1);
     tree->left = createNode(2);
     tree->right = createNode(3);
     tree->left->left = createNode(4);
     tree->left->right = createNode(5);
 
-    // tree = createTree(tree, 1, 5); // node, currentDepth, maxDepth
-
     printf("Inorder Traversal: ");
     inorderTraversal(tree); // Вывод: 4 2 5 1 3
     clearTree(tree);
 
-    return 0;
+    node* bsTree = createNode(20);
+
+    bsTree->left = createNode(10);
+    bsTree->left->left = createNode(5);
+    bsTree->left->right = createNode(15);
+
+    bsTree->right = createNode(30);
+    bsTree->right->left = createNode(25);
+    bsTree->right->right = createNode(35);
+
+    inorderTraversal(bsTree);  // Вывод: 5 10 15 20 25 30 35
+
+    node* resultNode = findNodeBSTree(bsTree, 15);
+    printf("%d bsTree\n", resultNode->value);
+    clearTree(bsTree);
+
+    return EXIT_SUCCESS;
 }
