801435569a5059bc452f560e390be6ab867e2605
Nikita Talovskiy <talovskiy@gmail.com>
2024-11-20 00:48:48 +0700

    WIP Arrays

--- 1.types/arrays/testArrays.c
+++ 1.types/arrays/testArrays.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
 #include "arrays.c"
 
+void testGetValByIndex() {
+    return;
+}
+
+void testGetIndexByVal() {
+    return;
+}
+
+void testInsertElement() {
+    return;
+}
+
 int main()
 {
+    testGetValByIndex();
+    testGetIndexByVal();
+    testInsertElement();
+    
     return 0;
 }
