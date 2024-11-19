7fbf0fa9bab23713b12f93a59267fc0f729e457c
Nikita Talovskiy <talovskiy@gmail.com>
2024-11-20 00:46:49 +0700

    WIP Arrays

--- 1.types/arrays/arrays.c
+++ 1.types/arrays/arrays.c
@@ -1,4 +1,50 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+int getValByIndex(int* array, int index) {
+    if (array != NULL && (sizeof(array[0]) / sizeof(int)) >= index)
+    {
+        return array[index];
+    }
+
+    return -1;
+}
+
+int getIndexByVal(int* array, int size, int value) {
+    if (array != NULL)
+    {
+        for (int i = 0; i < size; ++i)
+        {
+            if (array[i] == value)
+            {
+                return i;
+            }
+        }
+    }
+
+    return -1;
+}
+
+void insertElement(int **array, int *size, int index, int value) {
+    if (index < 0 || index > *size) {
+        printf("Invalid index\n");
+        return;
+    }
+
+    *array = realloc(*array, (*size + 1) * sizeof(int));
+    if (*array == NULL) {
+        printf("Memory allocation failed\n");
+        return;
+    }
+
+    for (int i = *size; i > index; i--) {
+        (*array)[i] = (*array)[i - 1];
+    }
+
+    (*array)[index] = value;
+    (*size)++;
+}
+
 
 int main()
 {
