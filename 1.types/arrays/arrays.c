8aaa10ff2dedbe8ddc6b95f3762352497729794e
Nikita Talovskiy <talovskiy@gmail.com>
2024-11-22 02:27:34 +0700

    WIP Arrays

--- 1.types/arrays/arrays.c
+++ 1.types/arrays/arrays.c
@@ -44,3 +44,23 @@ void insertElement(int **array, int *size, int index, int value) {
     (*array)[index] = value;
     (*size)++;
 }
+
+int binarySearch(int *arr, int size, int target) {
+    int left = 0, right = size - 1;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+
+        if (arr[mid] == target){
+            return mid;
+        }
+
+        if (arr[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    
+    return -1;
+}
